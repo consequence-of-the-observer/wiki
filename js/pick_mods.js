1c375255d8fc54aa226f1845dffc5b1b2d6f1ca1
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-03-22 13:21:42 -0700

    moved the mod_contents variable to it's own file

--- js/pick_mods.js
+++ js/pick_mods.js
@@ -1,26 +1,3 @@
-var mod_contents = {
-    "mod1": {
-        "title": "Mod 1",
-        "taken": false
-    },
-    "mod2": {
-        "title": "Mod 2",
-        "taken": false
-    },
-    "mod3": {
-        "title": "Mod 3",
-        "taken": false
-    },
-    "mod4": {
-        "title": "Mod 4",
-        "taken": false
-    },
-    "mod5": {
-        "title": "Mod 5",
-        "taken": false
-    }
-}
-
 function selectMods() {
     let mod1 = getRandomInt(5);
     console.log(mod1);
